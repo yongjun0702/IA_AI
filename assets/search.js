1713143dc17f62ac4ed7d05da78382a604617bbe
chowon442 <85076036+chowon442@users.noreply.github.com>
2024-06-02 06:22:04 +0000

    [feat] 검색어 강조 기능 추가

--- assets/search.js
+++ assets/search.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const searchInput = document.getElementById('searchInput');
     const resultsList = document.getElementById('results');
 
@@ -8,13 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             const pages = data;
 
-            searchInput.addEventListener('keyup', function() {
+            searchInput.addEventListener('keyup', function () {
                 const query = searchInput.value.toLowerCase();
                 resultsList.innerHTML = '';
 
                 if (query.length > 0) {
                     const results = pages.flatMap(page => {
-                        const matchingSentences = page.content.filter(sentence => sentence.toLowerCase().includes(query));
+                        const matchingSentences = page.content.filter(sentence => sentence.toLowerCase().includes(query)).map(sentence => {
+                            const highlightedSentence = sentence.replace(new RegExp(`(${query})`, 'gi'), '<span class="highlight">$1</span>');
+                            return highlightedSentence;
+                        });
                         if (matchingSentences.length > 0) {
                             return { ...page, matchingSentences };
                         }
@@ -31,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         const ul = document.createElement('ul');
                         page.matchingSentences.forEach(sentence => {
                             const sentenceLi = document.createElement('li');
-                            sentenceLi.textContent = sentence;
+                            sentenceLi.innerHTML = sentence;
                             ul.appendChild(sentenceLi);
                         });
                         li.appendChild(ul);
